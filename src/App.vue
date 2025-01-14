f966811bc984159fd258557828762424ee949b99
adamswan <melyling@sina.com>
2025-01-14 23:29:27 +0800

    初始化环境

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>123</div>
+</template>
+
+<script setup>
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+console.log(scene);
+</script>
+
+<style scoped></style>
