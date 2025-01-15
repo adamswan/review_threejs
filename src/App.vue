13425e5da81badc7198e41963936d85e480b952f
adamswan <melyling@sina.com>
2025-01-15 11:58:47 +0800

    创建一个会动的粉色立方体

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,60 @@
 <template>
-  <div>123</div>
+  <div></div>
 </template>
 
 <script setup>
 import * as THREE from "three";
 
+// 1、创建场景
 const scene = new THREE.Scene();
-console.log(scene);
+
+
+// 2、创建相机
+const camera = new THREE.PerspectiveCamera(
+  45, // 角度
+  window.innerWidth / window.innerHeight, // 相机的宽高比
+  1, // 近平面
+  1000 // 远平面
+);
+// 设置相机位置
+camera.position.set(0, 0, 20);
+
+
+// 3、创建渲染器
+const renderer = new THREE.WebGLRenderer();
+// 设置渲染器尺寸大小
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+
+// 4、将渲染器添加到body
+document.body.appendChild(renderer.domElement);
+
+
+// 5、添加一个立方体
+// 5.1、创建立方体对象
+const cubeGeometry = new THREE.BoxGeometry(3, 3, 3);
+// 5.2、创建材质对象
+const cubeMaterial = new THREE.MeshBasicMaterial({
+  color: 'pink', // 颜色
+  wireframe: false, // 是否是线框模式
+});
+// 5.3、创建网格模型对象
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+// 5.4、添加到场景中
+scene.add(cube);
+
+
+// 7、添加动画，让图形动起来
+function animation() {
+  cube.rotation.x = cube.rotation.x + 0.01;
+  cube.rotation.y = cube.rotation.y + 0.01;
+
+  // 6、渲染，传入场景和相机
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(animation);
+}
+animation()
 </script>
 
 <style scoped></style>
